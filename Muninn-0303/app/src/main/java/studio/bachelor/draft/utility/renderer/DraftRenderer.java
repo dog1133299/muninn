482b5f8b63d9bd3b00e75dd8db6c914d8f5ca383
unknown <>
2017-03-04 00:11:04 +0800

    0302+修正介面+doubletap

---
+++ Muninn-0303/app/src/main/java/studio/bachelor/draft/utility/renderer/DraftRenderer.java
@@ -0,0 +1,73 @@
+package studio.bachelor.draft.utility.renderer;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.util.List;
+
+import studio.bachelor.draft.Draft;
+import studio.bachelor.draft.utility.Position;
+import studio.bachelor.draft.utility.Renderable;
+import studio.bachelor.muninn.Muninn;
+
+/**
+ * Created by BACHELOR on 2016/02/24.
+ */
+public class DraftRenderer implements Renderable {
+    private Draft draft;
+    private Bitmap birdview;
+    private final Paint paint = new Paint(); //for image
+    private final Paint pathPaint = new Paint(); //for path(草稿線)
+    private boolean ableToPaint = true;
+
+    { //path會依據Paint的設定，呈現不同線條
+        pathPaint.setStrokeCap(Paint.Cap.ROUND);
+        pathPaint.setStrokeWidth(5.0f);
+        pathPaint.setStyle(Paint.Style.STROKE);
+    }
+
+    public DraftRenderer(Draft draft) {
+        this.draft = draft;
+    }
+
+    public void setBirdview(Uri uri) {
+        try {
+            birdview = MediaStore.Images.Media.getBitmap(Muninn.getContext().getContentResolver(), uri);
+        } catch (Exception e) {
+            Log.d("DraftRenderer", "setBirdview(Uri uri)" + e.toString());
+        }
+    }
+
+    public void setBirdview(Bitmap bitmap) {
+        birdview = bitmap;
+    }
+
+    public void setAbleToPaint(boolean b){ableToPaint = b;}
+    public boolean getAbleToPaint(){return ableToPaint;}
+    public Bitmap getBirdview() {
+        return birdview;
+    }
+
+    public void onDraw(Canvas canvas) {
+        Position translate = draft.layer.getTranslate();//?Jonas
+        float scale = draft.layer.getScale();
+        canvas.translate((float)translate.x, (float)translate.y);//位移
+        canvas.scale(scale, scale);
+        if(birdview != null)
+            canvas.drawBitmap(birdview, -birdview.getWidth() / 2, -birdview.getHeight() / 2, paint);
+
+        Path current_path = draft.getCurrentPath();
+        if(current_path != null)
+            canvas.drawPath(current_path, pathPaint);//?Jonas
+
+        List<Path> paths = draft.getPaths();
+        for (Path path : paths) //show all the gesture paths
+            canvas.drawPath(path, pathPaint);
+    }
+}
---
+++ Muninn-0303/app/src/main/java/studio/bachelor/draft/utility/renderer/builder/MarkerRendererBuilder.java
@@ -0,0 +1,99 @@
+package studio.bachelor.draft.utility.renderer.builder;
+
+import android.util.Log;
+
+import java.util.List;
+
+import studio.bachelor.draft.marker.LinkMarker;
+import studio.bachelor.draft.marker.Marker;
+import studio.bachelor.draft.utility.MapString;
+import studio.bachelor.draft.utility.Position;
+import studio.bachelor.draft.utility.renderer.primitive.CenterText;
+import studio.bachelor.draft.utility.renderer.primitive.Line;
+import studio.bachelor.draft.utility.renderer.primitive.Point;
+import studio.bachelor.draft.utility.renderer.primitive.Text;
+import studio.bachelor.draft.utility.renderer.MarkerRenderer;
+
+/**
+ * Created by BACHELOR on 2016/02/25.
+ */
+public class MarkerRendererBuilder extends RendererBuilder {
+    public MarkerRendererBuilder() {
+        super();
+    }
+
+    protected void createProductIfNull() {
+        if(product == null)
+            product = new MarkerRenderer();
+    }
+
+    //建立Marker與Renderer的關係
+    public MarkerRendererBuilder setReference(Marker marker) {
+        createProductIfNull();
+        MarkerRenderer renderer = (MarkerRenderer)product;
+        renderer.setReference(marker);
+        return this;
+    }
+
+    public MarkerRendererBuilder setPoint(Marker marker) {
+        createProductIfNull();
+        MarkerRenderer renderer = (MarkerRenderer)product;
+        Point primitive = new Point(marker.position);
+        renderer.primitives.add(primitive);
+        return this;
+    }
+
+    public MarkerRendererBuilder setLinkLine(LinkMarker marker) {
+        createProductIfNull();
+        MarkerRenderer renderer = (MarkerRenderer)product; //initial the line's parameter
+        Line primitive = new Line(marker.position, marker.getLink().position); //implements Renderable
+        Log.d("setLinkLine", "=====linked: (" + marker.getLink().refreshed_tap_position.x + ", " + marker.getLink().refreshed_tap_position.y + ") marker: (" + marker.refreshed_tap_position.x + ", " + marker.refreshed_tap_position.y + ")");
+        renderer.primitives.add(primitive);
+        return this;
+    }
+
+    /*public MarkerRendererBuilder setIcon(Marker marker, int resource) {
+        createProductIfNull();
+        MarkerRenderer renderer = (MarkerRenderer)product;
+        Icon primitive = new Icon(marker.position, resource);
+        renderer.primitives.add(primitive);
+        return this;
+    }*/
+
+    public MarkerRendererBuilder setText(String string, Position position) {
+        createProductIfNull();
+        MarkerRenderer renderer = (MarkerRenderer)product;
+        Text text = new Text(string, position);
+        renderer.primitives.add(text);
+        return this;
+    }
+
+    public MarkerRendererBuilder setText(String string, List<Position> positions) {
+        createProductIfNull();
+        MarkerRenderer renderer = (MarkerRenderer)product;
+        Text text = new CenterText(string, positions);
+        renderer.primitives.add(text);
+        return this;
+    }
+
+    public MarkerRendererBuilder setText(MapString string, Position position) {
+        createProductIfNull();
+        MarkerRenderer renderer = (MarkerRenderer)product;
+        Text text = new Text(string, position);
+        renderer.primitives.add(text);
+        return this;
+    }
+
+    public MarkerRendererBuilder setText(MapString string, List<Position> positions) {
+        createProductIfNull();
+        MarkerRenderer renderer = (MarkerRenderer)product;
+        Text text = new CenterText(string, positions); //implements Renderable
+        renderer.primitives.add(text);
+        return this;
+    }
+
+    public MarkerRendererBuilder clearProductCache() {
+        super.clearProductCache();
+        return (MarkerRendererBuilder)this;
+    }
+}
